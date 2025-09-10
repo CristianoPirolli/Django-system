3c0ce2b1890afc8beb89c04b180e306bca5298b6
Cristiano Rodrigues Pirolli <cristianopirolli@hotmail.com>
2025-09-10 19:45:53 -0300

    Style templates with Bootstrap

--- cadastro/forms.py
+++ cadastro/forms.py
@@ -6,12 +6,24 @@ class AnimalForm(forms.ModelForm):
     class Meta:
         model = Animal
         fields = ['sex', 'age', 'ear_tag_number', 'mother_ear_tag_number']
+        widgets = {
+            'sex': forms.Select(attrs={'class': 'form-select'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control'}),
+            'ear_tag_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'mother_ear_tag_number': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 
 class VaccineForm(forms.ModelForm):
     class Meta:
         model = Vaccine
         fields = ['name', 'application_date', 'second_dose', 'second_dose_date']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'application_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'second_dose': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'second_dose_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        }
 
     def clean(self):
         cleaned_data = super().clean()
